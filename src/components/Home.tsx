7c1486b2c77de62b20cfc0735e79e8283898d12d
Obasi Chizitere <blossomobasi2@gmail.com>
2024-04-29 12:43:38 +0100

    Responsiveness for Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,8 +6,8 @@ const HomePage = () => {
     <section className="h-full w-full bg-home-image bg-cover bg-center text-white">
       <NavBar />
 
-      <div className="px-36 pt-32 h-[calc(100vh-3.5rem)]">
-        <h1 className="font-lobster text-6xl pl-4 relative before:h-[calc(100%-1.5rem)] before:w-1 before:bg-gradient-to-b from-primary to-white before:absolute before:left-1">
+      <div className="lg:px-36 px-10 pt-32 h-[calc(100vh-3.5rem)]">
+        <h1 className="font-lobster lg:text-6xl text-4xl pl-4 relative before:h-[calc(100%-1.5rem)] before:w-1 before:bg-gradient-to-b from-primary to-white before:absolute before:left-1">
           We will move anything <br /> into any direction you <br /> want.
         </h1>
         <RequestQuoteButton className="ml-4 mt-5 bg-primary p-3 rounded-lg">
