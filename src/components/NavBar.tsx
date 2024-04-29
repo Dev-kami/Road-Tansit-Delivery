129ba18e3287b3a10b461b7ff29b1b1cde194c8c
Obasi Chizitere <blossomobasi2@gmail.com>
2024-04-29 12:42:03 +0100

    Responsiveness for navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,8 +4,8 @@ import RequestQuoteButton from "@/ui/RequestQuoteButton";
 const NavBar = () => {
   const data = navData;
   return (
-    <header className="px-36 h-14 flex bg-[rgba(40,54,24,0.3)]">
-      <nav className="flex items-center h-full w-full gap-16">
+    <header className="lg:px-36 px-10 h-14 flex bg-[rgba(40,54,24,0.3)]">
+      <nav className="flex items-center h-full w-full lg:gap-16 gap-5">
         {data.map((item) => (
           <ul key={item.title}>
             <li className="cursor-pointer">{item.title}</li>
@@ -13,7 +13,7 @@ const NavBar = () => {
         ))}
       </nav>
 
-      <RequestQuoteButton className="bg-primary w-40">
+      <RequestQuoteButton className="bg-primary w-40 ml-5 hidden sm:block">
         Request Quote
       </RequestQuoteButton>
     </header>
